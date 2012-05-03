cc26d356a043fb0ee7163833e1a6e96ff135bcea
xoomdev <xoomdevnet@gmail.com>
2012-05-03 14:53:42 -0500

    Initial push of Samsung source release

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Thu May 3 14:30:43 CDT 2012"
+#define LINUX_COMPILE_BY "xoomdev"
+#define LINUX_COMPILE_HOST "xoomdev-ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
